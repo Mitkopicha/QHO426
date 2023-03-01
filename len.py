5eb17b72e7a37daf8e9410f1480e61f8dd4c7dab
Mitkopicha <dimitar.dutchev@gmail.com>
2023-03-01 16:16:08 +0000

    len

---
+++ len.py
@@ -0,0 +1,9 @@
+# ASK USER FOR INPUT
+x = input("Please enter a phrase:\n")
+# CREATE CONTROL VARIABLE
+bops = 0
+
+while bops< len(x):  # USING LEN FUNCTION TO COUNT THE LETTERS IN X VARIABLE
+    bops=bops+1  # INCREASE THE CONTROL VARIABLE BY 1
+    print("Bop")
+
