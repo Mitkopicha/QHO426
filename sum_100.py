f5e9edbcea0ce12821c6505f8ce3fed39a6ca7f1
Mitkopicha <dimitar.dutchev@gmail.com>
2023-03-01 16:31:54 +0000

    sum_100

---
+++ sum_100.py
@@ -0,0 +1,15 @@
+# Display start message
+print("Calculating the sum of the first 100 numbers..")
+
+# Declare control variable
+i = 1
+
+# Calculate sum of first 100 numbers
+total = 0
+while i <= 100:
+    total = total + i
+    i = i + 1
+
+# Display result
+print("...Done! The answer is", total)
+
