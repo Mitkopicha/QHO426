aeee34b9ad1e12a5de7154319e80eb736d4ca826
Mitkopicha <dimitar.dutchev@gmail.com>
2023-03-07 11:47:53 +0000

    Added code to demonstrate the use of a user-defined function.

--- function_with_loop.py
+++ function_with_loop.py
@@ -3,10 +3,10 @@ def cross_bridge(steps):
     for step in range(steps):
        print("Crossed step")
          # Display message
-        if (steps > 5):
+    if (steps > 5):
             print("The bridge is collapsing!")
-        else:
-            print("we must keep going!")
+    else:
+                print("we must keep going!")
 
 
 cross_bridge(3)
--- simple_funcion.py
+++ simple_funcion.py
@@ -1,5 +1,7 @@
 def listen():
     s = input("What sound did I hear: ")
     print(f"That was a loud {s}!")
+    print()
+
 
 listen()
